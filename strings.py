6f773396b38b66233d7702848527df81b0244d09
rogerthatrob <rogerthatrob@gmail.com>
2020-07-06 19:23:11 -0400

    finished lecture 3

--- strings.py
+++ strings.py
@@ -18,3 +18,82 @@ if len(s1) == len(s2):
             if char1 == char2:
                 print("common letter")
                 break
+
+print('----'*10)
+
+s="abcdefgh"
+print(s[::-1]) #reverses the string
+
+print('----'*10)
+
+####################
+## EXAMPLE: while loops and strings
+## CHALLENGE: rewrite while loop with a for loop
+####################
+an_letters = "aefhilmnorsxAEFHILMNORSX"
+word = input("I will cheer for you! Enter a word: ")#
+times = int(input("Enthusiasm level (1-10): "))
+
+# i = 0
+# while i < len(word):
+#     char = word[i]
+for char in word:
+    if char in an_letters:
+        print("Give me an " + char + "! " + char)
+    else:
+        print("Give me a  " + char + "! " + char)
+    #i += 1
+print("What does that spell?")
+for i in range(times):
+    print(word, "!!!")
+
+print('----'*10)
+
+####################
+## EXAMPLE: approximate cube root 
+####################
+cube = 27
+#cube = 8120601
+#cube = 10000
+epsilon = 0.1
+guess = 0.0
+increment = 0.01
+num_guesses = 0
+# look for close enough answer and make sure
+# didn't accidentally skip the close enough bound
+while abs(guess**3 - cube) >= epsilon and guess <= cube:
+   guess += increment
+   num_guesses += 1
+print('num_guesses =', num_guesses)
+if abs(guess**3 - cube) >= epsilon:
+   print('Failed on cube root of', cube, "with these parameters.")
+else:
+   print(guess, 'is close to the cube root of', cube)
+
+print('----'*10)
+
+####################
+## EXAMPLE: bisection cube root (only positive cubes!)
+####################
+cube = 27
+#cube = 8120601
+# won't work with x < 1 because initial upper bound is less than ans
+#cube = 0.25
+epsilon = 0.01
+num_guesses = 0
+low = 0
+high = cube
+guess = (high + low)/2.0
+while abs(guess**3 - cube) >= epsilon:
+   if guess**3 < cube:
+       # look only in upper half search space
+       low = guess
+   else:
+       # look only in lower half search space
+       high = guess
+   # next guess is halfway in search space
+   guess = (high + low)/2.0
+   num_guesses += 1
+print('num_guesses =', num_guesses)
+print(guess, 'is close to the cube root of', cube)
+   
