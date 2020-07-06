1a313553a89a972f3fd48c7533d79d2eed558aef
rogerthatrob <rogerthatrob@gmail.com>
2020-07-06 13:14:47 -0400

    exercies problems from mit ocw python lecture 3

--- strings.py
+++ strings.py
@@ -1,1 +1,20 @@
 ## going to practice some python 
+#mit ocw python lecture 3
+s = "6.00 is 6.0001 and 6.0002"
+new_str = ""
+new_str += s[-1]
+new_str += s[0]
+new_str += s[4::30]    
+new_str += s[13:10:-1]
+print(new_str)
+
+print('----'*10)
+
+s1 = "mit u rock"
+s2 = "i rule mit"
+if len(s1) == len(s2):
+    for char1 in s1:
+        for char2 in s2:
+            if char1 == char2:
+                print("common letter")
+                break
