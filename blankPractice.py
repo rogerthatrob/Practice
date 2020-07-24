f3acfd68196aa7c801ef1ed8b38560b5715ac837
Robert Haire <RobertHaire@Marias-MacBook-Pro.local>
2020-07-24 19:20:28 -0400

    kinda works

--- blankPractice.py
+++ blankPractice.py
@@ -1,13 +1,22 @@
 #write a program that asks the user to input 10 integers, 
 # and then prints the largest odd number that was entered. 
 # if no odd number was entereed, it should print a message to that effect
-#will need to test if not divisble by 2 to tell if the number is odd
+# will need to test if not divisble by 2 to tell if the number is odd
 
-a_list = []
+big = 0
 i = 0
-while i<10:
-    a_list = int(input("enter a digit: "))
-    i = i+1
 
-print('left while loop')
+while i < 10:
+    i += 1
+    a_list = int(input("input an integer: "))
 
+    if a_list > big:# & a_list % 2 == 0:
+        print("a_list is bigger than big")
+        big = a_list
+        
+        if big % 2 != 0:
+            print("num is odd", big)
+
+        print("biggest num is: ", big, "\n")
+    else:
+        print("num ins't the biggest \n")
