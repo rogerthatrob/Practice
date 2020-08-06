bd7db5d0b409bd9da4e2bc5e762c3eb706edca4a
rogerthatrob <rogerthatrob@gmail.com>
2020-08-05 22:37:15 -0400

    a

--- classProject.py
+++ classProject.py
@@ -1,17 +1,17 @@
-#this is where project will be
+
 def AlG1(A,n,i):
     def INSERTION_SORT(A,n):
-    print(A[i])
+        print(A[i])
 
 
 def ALG2(A,n,i):
     def HEAPSORT(A,n):
-    print(A,i)
+        print(A,i)
 
 
 def ALG3(A,n,i):
-    x = def RANDOMIZED_SELECT(A,1,n,i):
-    print(x)    
+    x = RANDOMIZED_SELECT(A,1,n,i)
+            print(x)    
     
 
 def main():
