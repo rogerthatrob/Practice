50d70018df31bdd358f0af8e7b431e2678fead7d
rogerthatrob <rogerthatrob@gmail.com>
2020-11-04 19:45:45 -0500

    added inventory to mess around with

---
+++ invetory.py
@@ -0,0 +1,85 @@
+REDS		750ml
+Gason Malbec = 	2.9	
+apothic =	1.9	
+kj merlot =	3	
+meiomi	0.2	
+la crema	1.7	
+j. lohr	3.7	
+coatal cab	1.1	1L
+coatal merlot	1.3	1L
+WHITES		750ml
+chateau st michelle chard	1	
+ecco domani pinot g	2.7	
+santa marg	1.5	
+muscato	0.9	
+st suprey	1.9	
+kj chard	1.2	
+white haven 	1	
+maison belle claire	0.8	
+sonoma cutrer	1	
+coatal pinot g	1.5	1L
+coastal chard	0.8	1L
+CHAMP		750ml
+korbel 	1.3	
+korbel 	2	187.5
+moet brut	1	187.5
+chandon	3	187.5
+
+		
+		
+		
+		
+		
+		
+		
+		
+SPIRITS		1L
+jager	0.7	
+rumplemintz	1	
+van goh	3.2	
+goose	3.5	
+ketel	2	
+capt morgran	3.8	
+barcardi silver	2.2	
+johnnie black	6.7	
+jack daniels	3.1	
+jose cuervo	1.7	
+svedka	3.8	
+Bomab Gin	0.3	
+triple sec	0.5	
+sour apple	1	
+peach schnapps	0.9	
+don q limon	0.8	750ml
+ketel	0.4	
+titos	0.6	
+blue curacao	0.3	
+crop cucumber 	0.7	
+watermelon bols	0.3	
+campari	0.2	
+hendrix	0.9	
+bombay saphire	0.9	
+baileys	0.8	
+butter scotch schnapps	1	
+st germain	0.9	
+tanquery	0.7	
+kahula	0.5	
+three olives grape	0.8	
+three olives pineapple	0.9	
+goose pear	0.8	
+lychee	0.7	
+three olives vanilla	0.3	
+trader vicks	0.3	
+van goh esspresso	0.4	
+van goh acai	0.9	
+roses grenadine	0.9	
+tribuno dry	0.9	375ml
+tribuno sweet	0.9	375ml
+finlandia	0.1	
+bols pomegranate	0.7	
+finlandia grapefruit	0.5	
+absolute mango	1	
+don q coconut	0.1	
+don q passion	0.9	
+cruzan mango	0.8	
+bacardi pine	1	
+bacardi dragon	0.8	
