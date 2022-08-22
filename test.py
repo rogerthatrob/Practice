94ca18fdd4becfbe9cb083f4a7efb3e508fb3ea3
Robert Haire <rogerthatrob@gmail.com>
2022-08-22 14:49:12 -0400

    used test file to make sure scipy was working, it is working

--- test.py
+++ test.py
@@ -1,13 +1,12 @@
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-print(thisdict["year"])
-
-temp1 = []
-
-temp1 = thisdict["brand"]
-type(temp1)
-
-temp1.append("chevy")
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+import numpy as np
+import math
+mean = 0
+variance = 1
+sigma = math.sqrt(variance)
+x = np.linspace(-3, 3, 201)
+plt.plot(x, norm.pdf((x-mean)/sigma),linewidth=2.0, label='normal PDF')
+plt.plot(x, norm.cdf((x-mean)/sigma),linewidth=2.0, label='normal CDF')
+plt.legend(bbox_to_anchor=(.35,1))
+plt.show()
